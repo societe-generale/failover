cffa1af161598f780f765d93c8682d0d973c0600
Anand Manissery <anand.manissery@socgen.com>
2023-01-30 17:03:14 +0530

    feat: spring boot 3 migration (#49)
    * ci: java17 and spring boot 3 migration
    
    * fix: test case fix for jar version
    
    * ci: move ci to java 17
    
    * refactor: adapting autoconfiguration to spring boot 3 way

--- failover-core/src/test/java/com/societegenerale/failover/core/report/manifest/DefaultManifestInfoExtractorTest.java
+++ failover-core/src/test/java/com/societegenerale/failover/core/report/manifest/DefaultManifestInfoExtractorTest.java
@@ -42,7 +42,7 @@ class DefaultManifestInfoExtractorTest {
     @Test
     void shouldReturnTheManifestInfo() {
         Map<String,String> result = manifestInfoExtractor.extract("slf4j-api");
-        assertThat(result).containsEntry("lib-metadata-title", "slf4j-api").containsEntry("lib-metadata-version", "1.7.36");
+        assertThat(result).containsEntry("lib-metadata-title", "slf4j-api").containsEntry("lib-metadata-version", "2.0.6");
     }
 
     @Test
