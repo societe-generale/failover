afe55f7566aef8b71c07cc8810833e67576ab668
Anand Manissery <anand.manissery@gmail.com>
2026-05-26 13:05:39 +0530

    refactor: auto config refactor to avoid loading scheduler if not enabled

--- failover-spring-boot-autoconfigure/src/main/java/com/societegenerale/failover/processor/FailoverStoreBeanPostProcessor.java
+++ failover-spring-boot-autoconfigure/src/main/java/com/societegenerale/failover/processor/FailoverStoreBeanPostProcessor.java
@@ -8,15 +8,16 @@ import org.jspecify.annotations.Nullable;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import static com.societegenerale.failover.core.util.CastingUtils.cast;
+
 public class FailoverStoreBeanPostProcessor implements BeanPostProcessor {
 
     @Override
-    @SuppressWarnings("unchecked")
     public @Nullable Object postProcessBeforeInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
         if (bean instanceof FailoverStore
                 && !(bean instanceof FailoverStoreAsync)
                 && !(bean instanceof DefaultFailoverStore)) {
-            return new FailoverStoreAsync<>(new DefaultFailoverStore<>((FailoverStore<Object>) bean));
+            return new FailoverStoreAsync<>(new DefaultFailoverStore<>(cast(bean)));
         }
         return bean;
     }
