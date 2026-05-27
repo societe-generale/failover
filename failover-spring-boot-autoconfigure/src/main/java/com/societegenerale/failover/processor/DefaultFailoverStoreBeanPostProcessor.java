70cb83a515ad93834c18e96f2f2a6bd4dcfa4ee5
Anand Manissery <anand.manissery@gmail.com>
2026-05-27 10:24:32 +0530

    refactor: async store configuration added by conditional, default is true

--- failover-spring-boot-autoconfigure/src/main/java/com/societegenerale/failover/processor/DefaultFailoverStoreBeanPostProcessor.java
+++ failover-spring-boot-autoconfigure/src/main/java/com/societegenerale/failover/processor/DefaultFailoverStoreBeanPostProcessor.java
@@ -7,18 +7,24 @@ import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
 
 import static com.societegenerale.failover.core.util.CastingUtils.cast;
 
-public class FailoverStoreBeanPostProcessor implements BeanPostProcessor {
+public class DefaultFailoverStoreBeanPostProcessor implements BeanPostProcessor, Ordered {
 
     @Override
     public @Nullable Object postProcessBeforeInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
         if (bean instanceof FailoverStore
                 && !(bean instanceof FailoverStoreAsync)
                 && !(bean instanceof DefaultFailoverStore)) {
-            return new FailoverStoreAsync<>(new DefaultFailoverStore<>(cast(bean)));
+            return new DefaultFailoverStore<>(cast(bean));
         }
         return bean;
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
     }
 }
