86100783d0026d9ef05f1cd83792a83f9b5f0106
Anand Manissery <anand.manissery@gmail.com>
2026-05-27 11:14:53 +0530

    refactor: jdbc store with payload type handler

--- failover-store-jdbc/src/test/java/com/societegenerale/failover/store/handler/VarcharPayloadColumnHandlerTest.java
+++ failover-store-jdbc/src/test/java/com/societegenerale/failover/store/handler/VarcharPayloadColumnHandlerTest.java
@@ -1,5 +1,6 @@
 package com.societegenerale.failover.store.handler;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -25,12 +26,14 @@ class VarcharPayloadColumnHandlerTest {
     private final VarcharPayloadColumnHandler handler = new VarcharPayloadColumnHandler();
 
     @Test
-    void payloadType_returnsVarchar() {
+    @DisplayName("should return VARCHAR as the payload SQL type")
+    void payloadTypeReturnsVarchar() {
         assertThat(handler.payloadType()).isEqualTo(Types.VARCHAR);
     }
 
     @Test
-    void extractPayload_returnsStringFromResultSet() throws SQLException {
+    @DisplayName("should extract and return the string payload from the result set")
+    void extractPayloadReturnsStringFromResultSet() throws SQLException {
         when(resultSet.getString(PAYLOAD_COLUMN_NAME)).thenReturn(JSON_PAYLOAD);
 
         String result = handler.extractPayload(resultSet, PAYLOAD_COLUMN_NAME);
@@ -40,7 +43,8 @@ class VarcharPayloadColumnHandlerTest {
     }
 
     @Test
-    void extractPayload_nullValue_returnsNull() throws SQLException {
+    @DisplayName("should return null when the result set column value is null")
+    void extractPayloadNullValueReturnsNull() throws SQLException {
         when(resultSet.getString(PAYLOAD_COLUMN_NAME)).thenReturn(null);
 
         String result = handler.extractPayload(resultSet, PAYLOAD_COLUMN_NAME);
@@ -49,7 +53,8 @@ class VarcharPayloadColumnHandlerTest {
     }
 
     @Test
-    void extractPayload_emptyString_returnsEmpty() throws SQLException {
+    @DisplayName("should return empty string when the result set column value is empty")
+    void extractPayloadEmptyStringReturnsEmpty() throws SQLException {
         when(resultSet.getString(PAYLOAD_COLUMN_NAME)).thenReturn("");
 
         String result = handler.extractPayload(resultSet, PAYLOAD_COLUMN_NAME);
@@ -58,7 +63,8 @@ class VarcharPayloadColumnHandlerTest {
     }
 
     @Test
-    void extractPayload_whitespaceString_returnsAsIs() throws SQLException {
+    @DisplayName("should return whitespace string as-is without trimming")
+    void extractPayloadWhitespaceStringReturnsAsIs() throws SQLException {
         when(resultSet.getString(PAYLOAD_COLUMN_NAME)).thenReturn("   ");
 
         String result = handler.extractPayload(resultSet, PAYLOAD_COLUMN_NAME);
@@ -67,7 +73,8 @@ class VarcharPayloadColumnHandlerTest {
     }
 
     @Test
-    void extractPayload_usesExactColumnName() throws SQLException {
+    @DisplayName("should use the exact column name provided to query the result set")
+    void extractPayloadUsesExactColumnName() throws SQLException {
         when(resultSet.getString("MY_CUSTOM_COL")).thenReturn("data");
 
         String result = handler.extractPayload(resultSet, "MY_CUSTOM_COL");
@@ -78,7 +85,8 @@ class VarcharPayloadColumnHandlerTest {
     }
 
     @Test
-    void extractPayload_sqlException_propagates() throws SQLException {
+    @DisplayName("should propagate SQLException thrown by the result set")
+    void extractPayloadSqlExceptionPropagates() throws SQLException {
         when(resultSet.getString(PAYLOAD_COLUMN_NAME)).thenThrow(new SQLException("column not found"));
         assertThrows(SQLException.class, () -> handler.extractPayload(resultSet, PAYLOAD_COLUMN_NAME));
     }
